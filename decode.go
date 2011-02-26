12cc6ba4884352b817c2b5b304b9ad9390255401
Evan Shaw <edsrzf@gmail.com>
2011-02-27 02:27:57 +1300

    Simplify logic in preparation for some changes

--- decode.go
+++ decode.go
@@ -7,8 +7,10 @@ package bson
 import (
 	"bytes"
 	"encoding/binary"
+	"math"
 	"os"
 	"reflect"
+	"runtime"
 	"strings"
 	"time"
 )
@@ -38,53 +40,50 @@ type decodeState struct {
 	*bytes.Buffer
 }
 
-func (d *decodeState) decodeDoc(v interface{}) os.Error {
+func (d *decodeState) error(err os.Error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (d *decodeState) decodeDoc(v interface{}) {
 	val := reflect.NewValue(v)
-	for {
+	unboxing: for {
 		switch v := val.(type) {
 		case *reflect.InterfaceValue:
 			if v.IsNil() {
-				return &InvalidUnmarshalError{v.Type()}
+				d.error(&InvalidUnmarshalError{v.Type()})
 			}
 			val = v.Elem()
 		case *reflect.MapValue:
-			return d.decodeMapDoc(v)
+			d.decodeMapDoc(v)
+			break unboxing
 		case *reflect.PtrValue:
 			if v.IsNil() {
-				return &InvalidUnmarshalError{v.Type()}
+				d.error(&InvalidUnmarshalError{v.Type()})
 			}
 			val = v.Elem()
 		case *reflect.StructValue:
-			return d.decodeStructDoc(v)
+			d.decodeStructDoc(v)
+			break unboxing
 		default:
-			return &InvalidUnmarshalError{val.Type()}
+			d.error(&InvalidUnmarshalError{val.Type()})
 		}
 	}
-	panic("unreachable")
 }
 
-func (d *decodeState) decodeMapDoc(v *reflect.MapValue) os.Error {
+func (d *decodeState) decodeMapDoc(v *reflect.MapValue) {
 	mapType := v.Type().(*reflect.MapType)
 	_, stringKey := mapType.Key().(*reflect.StringType)
 	if !stringKey {
-		return os.NewError("maps need a string key type")
+		d.error(&InvalidUnmarshalError{v.Type()})
 	}
 	elType := mapType.Elem()
-	// discard total length; it doesn't help us
-	d.Next(4)
-	kind, err := d.ReadByte()
-	for kind > 0 && err == nil {
-		var key string
-		key, err = d.readCString()
-		if err != nil {
-			break
-		}
 
-		var val interface{}
-		val, err = d.decodeElem(kind)
-		if err != nil {
-			break
-		}
+	kind, key, b := d.readChunk()
+	for kind > 0 {
+		val := d.decodeElem(kind, b)
+
 		refVal := reflect.NewValue(val)
 		var vType reflect.Type
 		if refVal != nil {
@@ -96,28 +95,16 @@ func (d *decodeState) decodeMapDoc(v *reflect.MapValue) os.Error {
 			refVal = iVal
 		}
 		v.SetElem(reflect.NewValue(key), refVal)
-		kind, err = d.ReadByte()
+		kind, key, b = d.readChunk()
 	}
-	return err
 }
 
-func (d *decodeState) decodeStructDoc(v *reflect.StructValue) os.Error {
+func (d *decodeState) decodeStructDoc(v *reflect.StructValue) {
 	st := v.Type().(*reflect.StructType)
-	// discard total length; it doesn't help us
-	d.Next(4)
-	kind, err := d.ReadByte()
-	for kind > 0 && err == nil {
-		var key string
-		key, err = d.readCString()
-		if err != nil {
-			break
-		}
 
-		var val interface{}
-		val, err = d.decodeElem(kind)
-		if err != nil {
-			break
-		}
+	kind, key, b := d.readChunk()
+	for kind > 0 {
+		val := d.decodeElem(kind, b)
 
 		var fieldVal reflect.Value
 		var f reflect.StructField
@@ -143,147 +130,193 @@ func (d *decodeState) decodeStructDoc(v *reflect.StructValue) os.Error {
 		}
 
 		refVal := reflect.NewValue(val)
-		var vType reflect.Type
-		if refVal != nil {
-			vType = refVal.Type()
-		}
-		fieldType := fieldVal.Type()
-		if fieldType != vType {
-			switch fieldType.(type) {
-			case *reflect.InterfaceType:
-			}
-		}
 		fieldVal.SetValue(refVal)
-		kind, err = d.ReadByte()
+
+		kind, key, b = d.readChunk()
 	}
-	return err
 }
 
-func (d *decodeState) readCString() (string, os.Error) {
+func (d *decodeState) readCString() string {
 	s, err := d.ReadString(0)
-	return s[:len(s)-1], err
+	d.error(err)
+	return s[:len(s)-1]
 }
 
-func (d *decodeState) readString() (string, os.Error) {
+func (d *decodeState) readString() string {
 	var l int32
 	err := binary.Read(d, order, &l)
-	if err != nil {
-		return "", err
-	}
-	b := make([]byte, l-1)
+	d.error(err)
+	b := make([]byte, l)
 	d.Read(b)
 	// discard null terminator
-	d.ReadByte()
-	return string(b), nil
+	return string(b[:l - 1])
 }
 
-func (d *decodeState) decodeElem(kind byte) (interface{}, os.Error) {
+const (
+	doubleNull = -1
+	lengthEncodedMinus = -2
+	lengthEncoded = -6
+	lengthEncodedPlus = -7
+)
+
+var lengths = []int32 {
+	0x01: 8,
+	0x02: lengthEncoded,
+	0x03: lengthEncodedMinus,
+	0x04: lengthEncodedMinus,
+	0x05: lengthEncodedPlus,
+	0x07: 12,
+	0x08: 1,
+	0x09: 8,
+	0x0A: 0,
+	0x0B: doubleNull,
+	0x0D: lengthEncoded,
+	0x0E: lengthEncoded,
+	0x0F: lengthEncodedMinus,
+	0x10: 4,
+	0x12: 8,
+	0x7F: 0,
+	0xFF: 0,
+}
+
+func (d *decodeState) readChunk() (kind byte, key string, b []byte) {
+	kind, err := d.ReadByte()
+	d.error(err)
+	if kind == 0 {
+		return
+	}
+
+	key = d.readCString()
+	switch n := lengths[kind]; {
+	case n == doubleNull:
+		s1, err := d.ReadString(0)
+		d.error(err)
+		s2, err := d.ReadString(0)
+		d.error(err)
+		b = []byte(s1 + s2)
+	case n < 0:
+		// length-encoded with a possible offset
+		var l int32
+		binary.Read(d, order, &l)
+		b = make([]byte, l + (lengthEncoded - n))
+		d.Read(b)
+	case n == 0:
+	default:
+		b = make([]byte, n)
+		d.Read(b)
+	}
+	return
+}
+
+func (d *decodeState) decodeElem(kind byte, b []byte) interface{} {
 	switch kind {
 	case 0x01:
 		// float
-		var f float64
-		err := binary.Read(d, order, &f)
-		return f, err
+		f := math.Float64frombits(order.Uint64(b))
+		return f
 	case 0x02:
 		// string
-		return d.readString()
+		return string(b[:len(b)-1])
 	case 0x03:
 		// document
 		m := make(map[string]interface{})
-		err := d.decodeDoc(m)
-		return m, err
+		d2 := &decodeState{bytes.NewBuffer(b)}
+		d2.decodeDoc(m)
+		return m
 	case 0x04:
 		// array
 		// byte length doesn't help
-		d.Next(4)
+		d2 := &decodeState{bytes.NewBuffer(b)}
 		var s []interface{}
-		kind, err := d.ReadByte()
-		for kind > 0 && err == nil {
-			// discard key
-			n := byte(1)
-			for n != 0 {
-				n, _ = d.ReadByte()
-			}
-
+		kind, _, b := d2.readChunk()
+		for kind > 0 {
 			var el interface{}
-			el, err = d.decodeElem(kind)
+			el = d2.decodeElem(kind, b)
 			s = append(s, el)
-			kind, err = d.ReadByte()
+			kind, _, b = d2.readChunk()
 		}
-		return s, err
+		return s
 	case 0x05:
 		// binary data
-		var l int32
-		err := binary.Read(d, order, &l)
 		// assuming binary/generic data; discarding actual kind
-		d.ReadByte()
-		b := make([]byte, l)
-		d.Read(b)
-		return b, err
+		// TODO: consider making a copy of this data so that we won't
+		// be holding references to potentially large blocks of
+		// memory
+		return b[1:]
 	case 0x07:
 		// object ID
 		var o ObjectId
-		_, err := d.Read(o[:])
-		return &o, err
+		copy(o[:], b)
+		return &o
 	case 0x08:
 		// boolean
-		b, err := d.ReadByte()
-		return b != 0, err
+		return b[0] != 0
 	case 0x09:
 		// time
-		var t int64
-		err := binary.Read(d, order, &t)
-		return time.SecondsToUTC(t), err
+		t := int64(order.Uint64(b))
+		return time.SecondsToUTC(t)
 	case 0x0A:
 		// null
-		return nil, nil
+		return nil
 	case 0x0B:
 		// regex
-		r, err := d.readCString()
+		pos := bytes.IndexByte(b, 0)
+		// TODO: consider copying
+		r := string(b[:pos])
 		// discard options
-		d.readCString()
-		return &Regexp{Expr: r}, err
+		return &Regexp{Expr: r}
 	case 0x0D:
 		// javascript
-		j, err := d.readString()
-		return &JavaScript{Code: j}, err
+		return &JavaScript{Code: string(b[:len(b)-1])}
 	case 0x0E:
 		// symbol
-		s, err := d.readString()
-		return Symbol(s), err
+		return Symbol(b[:len(b)-1])
 	case 0x0F:
 		// javascript w/ scope
-		d.Next(4)
-		code, err := d.readString()
-		if err != nil {
-			return nil, err
-		}
+		d2 := &decodeState{bytes.NewBuffer(b)}
+		code := d2.readString()
+		// discard length
+		d2.Next(4)
 		scope := make(map[string]interface{})
-		err = d.decodeDoc(scope)
-		return &JavaScript{code, scope}, err
+		d2.decodeDoc(scope)
+		return &JavaScript{code, scope}
 	case 0x10:
 		// int32
-		var i int32
-		err := binary.Read(d, order, &i)
-		return i, err
+		return int32(order.Uint32(b))
 	case 0x12:
 		// int64
-		var i int64
-		err := binary.Read(d, order, &i)
-		return i, err
+		return int64(order.Uint64(b))
 	case 0x7F:
 		// max key
-		return MaxKey{}, nil
+		return MaxKey{}
 	case 0xFF:
 		// min key
-		return MinKey{}, nil
+		return MinKey{}
 	default:
-		panic("Unsupported type")
+		panic("unsupported type")
 	}
-	return nil, nil
+	return nil
 }
 
-func Unmarshal(data []byte, v interface{}) os.Error {
+func Unmarshal(data []byte, v interface{}) (err os.Error) {
+	defer func() {
+		if r := recover(); r != nil {
+			switch rval := r.(type) {
+			case runtime.Error:
+				panic(r)
+			case os.Error:
+				err = rval
+			case string:
+				err = os.NewError(rval)
+			default:
+				panic(r)
+			}
+		}
+	}()
+
 	d := &decodeState{bytes.NewBuffer(data)}
-	return d.decodeDoc(v)
+	// discard doc length -- it doesn't help
+	d.Next(4)
+	d.decodeDoc(v)
+	return
 }
